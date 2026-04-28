1a981641c1e1bff652166db46aa96ae554a3ecf8
Daniel <danibaranco@gmail.com>
2026-04-28 21:30:14 +0200

    Configure GitHub Pages build output and publish path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Lorem_Fistrum/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 });
